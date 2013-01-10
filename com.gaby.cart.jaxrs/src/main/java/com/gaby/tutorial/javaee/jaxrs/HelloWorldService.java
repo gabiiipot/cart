106d553dd0a6074f265b8146d0b831018bef38c1
Gabriela Potanc <gabiiipot@gmail.com>
2013-01-10 15:29:53 +0200

    jax rs

--- com.gaby.cart.jaxrs/src/main/java/com/gaby/tutorial/javaee/jaxrs/HelloWorldService.java
+++ com.gaby.cart.jaxrs/src/main/java/com/gaby/tutorial/javaee/jaxrs/HelloWorldService.java
@@ -4,17 +4,12 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
-@Path("/echo")
+@Path("/sayhello")
 public class HelloWorldService {
-
-//	@EJB
-//	private IBasicService service;
 
 	@GET
 	@Path("/{message}")
 	public String echo(final @PathParam(value="message") String message) {		
-		return "hello gabi";
+		return "hello " + message;
 	}
-	
-
 }
